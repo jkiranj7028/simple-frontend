47bf3fb9bde4c07204721c08fd3c3b8bb9839b95
jkiranj7028@gmail.come <you@example.com>
2025-08-19 22:20:48 +0530

    initial frontend commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from Frontend</h1>
+      <p>Connected to backend API will be next step...</p>
+    </div>
+  );
+}
+
+export default App;
